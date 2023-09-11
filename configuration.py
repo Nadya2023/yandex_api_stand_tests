aebe8499910bd183c8627f7a5f545b00757e7010
Nadegda <n9089152888@gmail.com>
2023-09-11 10:38:24 +0500

    My first commit

---
+++ configuration.py
@@ -0,0 +1,7 @@
+URL_SERVICE = "https://a00db24f-1e5b-4113-b714-5d2e1e1d285c.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
